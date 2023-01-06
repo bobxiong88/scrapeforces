7116f2c2e78cc9d24677089f2b154de6e29bd8c2
bobxiong88 <bobxiong88@gmail.com>
2023-01-05 22:05:21 -0800

    python stuff

--- app.py
+++ app.py
@@ -1,9 +1,8 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, send_file
 from flask_executor import Executor
 import json
 from time import sleep
 import bs4
-import csv
 import time
 import os
 import requests
@@ -11,34 +10,101 @@ import random
 import shutil
 import mysql.connector as mysql
 import traceback
+import flask
 
 HOST = "sql9.freemysqlhosting.net" # or "domain.com"
 DATABASE = "sql9587489"
 USER = "sql9587489"
 PASSWORD = "cxs48SNIsG"
 mydb = mysql.connect(host=HOST, database=DATABASE, user=USER, password=PASSWORD)
+'''
+mydb = mysql.connect(
+    host = "sql.freedb.tech",
+    user = "freedb_okayman",
+    password = "Z*&5a@@DMwwSnH5",
+    database = "freedb_scrapeforces")
+'''
 print("Connected to:", mydb.get_server_info())
 
-mycursor = mydb.cursor()
+mycursor = mydb.cursor(buffered = True)
 
 app = Flask(__name__)
 app.debug = True
 
 executor = Executor(app)
 
+end = False
+
 @app.route("/start", methods = ['get'])
 def post():
-    handle = request.args.get('handle')
-    status = request.args.get('status')
-    # OK = AC
-    # RUNTIME_ERROR = RTE
-    # WRONG_ANSWER = WA
-    # TIME_LIMIT_EXCEEDED = TLE
-    unique = request.args.get('unique')
-    # "Yes" = unique
-    rid = hash(handle+status+unique+str(random.randint(1,int(1e9)+7)))
-    executor.submit(get_submissions,handle,status,unique,rid)
-    return json.dumps({"rid": rid})
+    try:
+        handle = request.args.get('handle')
+        status = request.args.get('status')
+        unique = request.args.get('unique')
+        
+        rid = hash(handle+status+unique+str(random.randint(1,int(1e9)+7)))%(int(1e9)+7)+random.randint(1,int(1e10))
+
+        mycursor.execute("INSERT INTO progress (rid, status, current, total) VALUES (%s, %s, %s, %s)", (rid, "starting", 0, 0))
+        mydb.commit()
+        time.sleep(1)
+        
+        executor.submit(get_submissions,handle,status,unique,rid)
+        
+        res = flask.jsonify({"rid": rid})
+        res.headers.add('Access-Control-Allow-Origin', '*')
+        return res
+    except Exception:
+        traceback.print_exc()
+
+
+@app.route("/get_status", methods = ['get'])
+def get_status():
+    mid = request.args.get('rid')
+    print("okay status id is ", mid)
+    cur = (mid, 'failed', 100,100)
+    try:
+        print("BEFORE")
+        mycursor.execute("SELECT * FROM progress")
+        print("DURING")
+        for x in mycursor:
+            print(tuple(x))
+            if str(x[0]) == str(mid):
+                cur = tuple(x)
+                break
+        print("AFTER")
+    except Exception:
+        print("there is error in get_status")
+        traceback.print_exc()
+    print("cur is ", cur)
+    res = {"rid":cur[0],
+           "status":cur[1],
+           "current":cur[2],
+           "total":cur[3]}
+    res = flask.jsonify(res)
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
+
+@app.route("/download", methods = ['get'])
+def download():
+    rid = request.args.get('rid')
+    fname = str(rid)+'.zip'
+    res = send_file(fname,fname)
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
+
+@app.route("/done", methods = ['get'])
+def done():
+    rid = request.args.get('rid')
+    mycursor.execute("DELETE FROM progress WHERE rid = %s", (rid,))
+    mydb.commit()
+    if os.path.isdir(str(rid)):
+        shutil.rmtree(str(rid))
+    if os.path.exists(str(rid)+'.zip'):
+        os.remove(str(rid)+'.zip')
+    res = {"hi":0}
+    res = flask.jsonify(res)
+    res.headers.add('Access-Control-Allow-Origin', '*')
+    return res
 
 @app.errorhandler(404)
 def page_not_found(error):
@@ -46,6 +112,7 @@ def page_not_found(error):
 
 def get_submissions(handle, status, unique, rid):
     try:
+        end = False
         # in case people troll
         
         '''
@@ -54,35 +121,36 @@ def get_submissions(handle, status, unique, rid):
         '''
 
         # uniqueness
+        print(handle,status,unique)
         unique = unique=='Yes'
+        print("processing:", handle, status, unique)
 
         # path to submission folder
-        path = "submissions\\"
+        path = "{}\\".format(rid)
 
         # submission limit
-        limit = 10000
+        limit = 10
         
         # get submission ids
         handleurl = "https://codeforces.com/api/user.status?handle={}&from=1&count={}".format(handle, limit)
         response = requests.get(handleurl)
-        submissions = json.loads(response.text)['result']
-    
-        # Create
-        if len(submissions) == 0:
+        stat = json.loads(response.text)['status']
+        
+        if stat == 'FAILED':
             print("here")
-            mycursor.execute("INSERT INTO progress (rid, status, current, total) VALUES (%s, %s, %s, %s)", (rid, "failed", 0, 0))
+            mycursor.execute("UPDATE progress SET status = %s WHERE rid = %s", ("failed", rid))
             mydb.commit()
             return
-
-
-
+        
+        submissions = json.loads(response.text)['result']
 
         # check for existing files
         if os.path.isdir(path):
-            shutil.rmtree('submissions')
-        os.mkdir('submissions')
-        
-        print("submissions created")
+            print("exist")
+            shutil.rmtree(str(rid))
+        os.mkdir(str(rid))
+        print("made", str(rid))
+        print("submissions created",rid)
 
         # pause to not get rate limited by the cf server 
         time.sleep(1)
@@ -98,6 +166,7 @@ def get_submissions(handle, status, unique, rid):
         prev = set()
         nxt = []
         for sub in submissions:
+            if end: break
             name = str(sub['contestId'])+sub['problem']['index']
             if name in prev:
                 continue
@@ -105,8 +174,8 @@ def get_submissions(handle, status, unique, rid):
             prev.add(name)
             exist[name] = 0
         if unique: submissions = nxt[:]
-
-        mycursor.execute("INSERT INTO progress (rid, status, current, total) VALUES (%s, %s, %s, %s)", (rid, "starting", 0, len(submissions)))
+        
+        mycursor.execute("UPDATE progress SET status = %s, current = %s, total = %s WHERE rid = %s", ("downloading", 0, len(submissions), rid))
         mydb.commit()
 
         print("# of subs: ", len(submissions))
@@ -117,8 +186,11 @@ def get_submissions(handle, status, unique, rid):
         for submission in submissions[:]:
             # Update
             sub_count += 1
+            
+            time.sleep(1)
             mycursor.execute("UPDATE progress SET status = %s, current = %s WHERE rid = %s", ("downloading", sub_count, rid))
             mydb.commit()
+            time.sleep(1)
 
             print("__________________________________________________________")
             # basic information about submission
@@ -183,18 +255,11 @@ def get_submissions(handle, status, unique, rid):
 
             # increment for not unique case and keeping track of already visited
             exist[fname] += 1
-            
-            '''
-            Daniel do your SQL shit here
-
-            basically send to "rid" how many files you've gone through, like 69 out of 420
-            so i can use it to make progress bar
-
-            Send it in the form of database[rid] = {"status": "downloading", "payload": (69,420)}
-            '''
 
             # pause or else codeforces big stupid
-            time.sleep(3)
+            time.sleep(1)
+        
+        shutil.make_archive(str(rid), 'zip', path)
         
         # Finished
         mycursor.execute("UPDATE progress SET status = %s, current = %s WHERE rid = %s", ("finished", len(submissions), rid))
@@ -202,12 +267,13 @@ def get_submissions(handle, status, unique, rid):
 
         # check if it worked
         mycursor.execute("SELECT * FROM progress")
-
         for x in mycursor:
             print(x)
+            
     except Exception:
         traceback.print_exc()
     
 # replace 192.168.56.1 with your actual IPv4 address
 # flask run -h 192.168.56.1
 # http://192.168.56.1:5000
+# http://192.168.56.1:5000/start?handle=bobxiong88&status=OK&unique=Yes
